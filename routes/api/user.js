e6678595be2fa98e3edbd4f7003f683ca9feeac5
Anders <anders_leung@hotmail.com>
2017-09-14 16:18:58 -0700

    Updated user api.

--- routes/api/user.js
+++ routes/api/user.js
@@ -6,11 +6,22 @@ var User = require('../../models/User');
 
 var UserService = {
     findUser : function(params, callback) {
-        User.findOne(params).lean().exec(function(err, client) {
+        User.findOne(params).lean().exec(function(err, user) {
             if (err) {
                 callback(err);
             } else {
-                callback(err, client);
+                callback(err, user);
+            }
+        });
+    },
+
+    getAllUsers : function(callback) {
+        User.find({}).lean().exec(function(err, users) {
+            if (err) {
+                callback(err);
+            }
+            else {
+                callback(err, users);
             }
         });
     }
