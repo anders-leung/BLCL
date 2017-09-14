e925eb279778db4cc537118a32ca2e70bdbc9b8e
Anders <anders_leung@hotmail.com>
2017-09-14 16:22:48 -0700

    Implemented the users route.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var async = require('async');
 var router = express.Router();
+
+var ClientService = require('./api/client');
+var UserService = require('./api/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    UserService.getAllUsers(function(err, users) {
+        var dict = {};
+        async.each(users, function(user, callback1) {
+            ClientService.findClients(user.clients, function(err, clients) {
+                if (err) {
+                    callback1(err);
+                } else {
+                    dict[user.initials] = clients;
+                    callback1();
+                }
+            });
+        }, function() {
+            res.render('users', { role: req.session.role, list: dict });
+        });
+    });
 });
 
 module.exports = router;
