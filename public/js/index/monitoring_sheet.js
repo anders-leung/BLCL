f93e6387f02c83d527c47fadb74743c68b04b2bd
Anders <anders_leung@hotmail.com>
2017-09-15 17:44:36 -0700

    Fixed small things across all the public js files, monitoring_sheet had largest changes to reflect socketio content.

--- public/js/index/monitoring_sheet.js
+++ public/js/index/monitoring_sheet.js
@@ -1,64 +1,61 @@
 /**
  * Created by ander on 2017-06-06.
  */
+function getField(column) {
+    var header = $('thead tr').find('th:nth-child(' + column + ')').html();
 
-function getAttribute(column_number) {
-    if (column_number == 10) {
-        return 'pickedUp';
-    } else if (column_number == 19) {
-        return 'preparerDone';
-    } else if (column_number == 21) {
-        return 'checkerDone';
-    } else if (column_number == 23) {
-        return 'readyForPickup';
-    } else if (column_number == 25) {
-        return 'pickupOk';
-    } else if (column_number == 26) {
-        return 'pytReceived';
-    } else if (column_number == 28) {
-        return 'prcLastYear';
+    switch (header) {
+        case 'Picked Up':
+            return 'pickedUp';
+        case 'Preparer Done':
+            return 'preparerDone';
+        case 'Checker Done':
+            return 'checkerDone';
+        case 'Outstanding Info':
+            return 'outstandingInfo';
+        case 'Remarks':
+            return 'remarks';
+        case 'Ready for Pickup':
+            return 'readyForPickup';
+        case 'Call Date':
+            return 'callDate';
+        case 'Pickup OK':
+            return 'pickupOk';
+        case 'PYT Received':
+            return 'pytReceived';
+        case 'Things to do After Pickup':
+            return 'thingsToDo';
+        case 'PRC Last Year':
+            return 'prcLastYear';
     }
-    return null;
-}
-
-function toggleSpan(span) {
-    if ($(span).hasClass('fa-check')) {
-        $(span).removeClass('fa-check');
-        $(span).addClass('fa-times');
-        return false;
-    } else {
-        $(span).removeClass('fa-times');
-        $(span).addClass('fa-check');
-        return true;
-    }
-}
-
-function inputToHtml(input, inputType) {
-    var value = $(input).val();
-    var cell = $(input).parent();
-    if (inputType == "date-edit") {
-        $(cell).toggleClass('date-edit');
-    } else {
-        $(cell).toggleClass('edit)');
-    }
-    cell.html(value);
-    resetTable();
-}
-
-function getScroll() {
-    var left = $(window).scrollLeft();
-    var down = $(window).scrollTop();
-    return left + ',' + down;
 }
 
 function setTable() {
-    $('table').DataTable({
+    $('table tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" />' );
+    } );
+
+    var table = $('table').DataTable({
         'paging': false,
         'info': false,
         'select': true,
         'scrollX': true,
-        'scrollY': '65vh'
+        'scrollY': '60vh'
     });
+
+    // Apply the search
+    table.columns().every( function () {
+        var that = this;
+
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
 }
 
 function resetTable() {
@@ -67,30 +64,81 @@ function resetTable() {
 }
 
 $(document).ready(function() {
-    setTable();
+    var socket = io();
 
-    if (scroll) {
-        $(window).scrollLeft(parseInt(scroll.split(',')[0]));
-        $(window).scrollTop(parseInt(scroll.split(',')[1]));
+    socket.on('monitoring sheet update', updateEvent);
+
+    function updateEvent(data) {
+        updateHtml(data.row, data.column, data.value, data.type);
     }
+
+    function updateHtml(row, column, value, type, emit) {
+        var rowHtml = $('tbody tr').eq(row);
+        var cell = $(rowHtml).find('td:nth-child(' + column + ')');
+        if (typeof(value) === 'boolean') {
+            var i = $(cell).find('i');
+            i.toggleClass('fa-check');
+            i.toggleClass('fa-times');
+        } else {
+            if (type == 'date') {
+                $(cell).toggleClass('date-edit');
+            } else if (type == 'edit') {
+                $(cell).toggleClass('edit)');
+            }
+            cell.html(value);
+        }
+
+        if (!emit) { return; }
+
+        var phone_number = $(rowHtml).find('td:nth-child(2)').html();
+        var field = getField(column);
+        socket.emit('monitoring sheet update', {
+            row: row,
+            column: column,
+            type: type,
+            phone_number: phone_number,
+            field: field,
+            value: value
+        });
+    }
+
+    function assignJobs() {
+        console.log("assigning jobs");
+        $('tbody tr').each(function() {
+            var phone_number = $(this).find('td:nth-child(' + 2 + ')').html();
+            var preparer = $(this).find('td:nth-child(' + 19 + ')').html();
+
+            socket.emit('assigning jobs', {
+                phone_number: phone_number,
+                preparer: preparer
+            });
+        });
+    }
+
+    setTable();
 
     $('tr td').not('.toggle, .edit, .date-edit').dblclick(function() {
         window.location = $(this).parent().data('href');
     });
 
+    $('#assignJobs').click(function() {
+        assignJobs();
+    });
+
     $('.toggle').click(function() {
-        var value = $(this).find('span');
-        var pathName = $(this).parent().data('href').split('/')[2];
-        var col = $(this).parent().children().index($(this));
-        var attr = getAttribute(col);
-        var info = pathName + ',' + attr + ',' + toggleSpan($(this).find('span')) + ',' + getScroll();
-        $('#post').val(info);
-        $('#submit').trigger('click');
+        var column = $(this).index();
+        var row = $(this).closest('tr').index();
+        var check = $(this).find('i');
+        var value = true;
+        if ($(check).hasClass('fa-check')) {
+            value = false;
+        }
+        updateHtml(row, column + 1, value, null, true);
     });
 
     $('.edit').click(function(e) {
         var value = $(this).html();
-        var input = $("<input class='form-control' type='text' onfocusout='inputToHtml(this, \"" + 'edit' + "\")' value='" + value + "'>");
+        var input = $("<input class='form-control' type='text' value='" + value + "'>");
         $(this).html(input);
         $(this).toggleClass('edit');
         $(input).focus();
@@ -101,14 +149,21 @@ $(document).ready(function() {
 
     $('.edit, .date-edit').keypress(function(e) {
         if (e.which == 13) {
-            inputToHtml($(this).find('input'), $(e.target));
+            var column = $(this).index();
+            var row = $(this).closest('tr').index();
+            var value = $(this).find('input').val();
+            var type = 'edit';
+            if (column == 25) {
+                type = 'date-edit';
+            }
+            updateHtml(row, column + 1, value, type, true);
             return false;
         }
     });
 
     $('.date-edit').click(function(e) {
         var value = $(this).html();
-        var date = $("<input class='form-control' type='date' onfocusout='inputToHtml(this, \"" + 'date-edit' + "\")' value='" + value + "'>");
+        var date = $("<input class='form-control' type='date' value='" + value + "'>");
         $(this).html(date);
         $(date).focus();
         $(this).toggleClass('date-edit');
