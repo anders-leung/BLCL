7fa2dd47ce2870f07ab3fa7280544ca7471be051
Anders <anders_leung@hotmail.com>
2017-10-30 16:35:56 -0700

    Uncommented setup.

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-//var setup = require('./routes/utils/setup');
-//setup();
+var setup = require('./routes/utils/setup');
+setup();
 
 /*
 var ClientService = require('./modules/client');
