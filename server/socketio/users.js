8ee8b876c2eca8a1bbb00f995ea31cacf69bda76
Anders <anders_leung@hotmail.com>
2018-01-21 17:11:19 -0800

    Removed all caps for the manage_users page

---
+++ server/socketio/users.js
@@ -0,0 +1,31 @@
+/**
+ * Created by ander on 2017-11-18.
+ */
+var UserService = require('../modules/user');
+
+var UsersSocket = {
+    addUser : function(socket) {
+        socket.on('add user', function (data) {
+            console.log('add user', data);
+            var name = data.user[0].split('@')[0];
+            var user = {
+                email: data.user[0],
+                initials: data.user[1],
+                role: data.user[2],
+                password: name
+            };
+            UserService.createUser(user, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        });
+    }, updateUser : function(socket) {
+        socket.on('update user', function(data) {
+            console.log('update user', data);
+            UserService.updateUser()
+        })
+    }
+};
+
+module.exports = UsersSocket;
