358dbf9ad04ccbda93fd63e755b3db31c1c2d8e4
Anders <anders.yb.leung@gmail.com>
2018-02-10 02:07:24 -0800

    Added the initials list.

--- server/routes/t1/monitoring_sheet.js
+++ server/routes/t1/monitoring_sheet.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var CookieService = require('./../utils/cookies');
 var ClientService = require('../../modules/client');
+var UserService = require('../../modules/user');
 
 /* GET home page. */
 router.get('/', CookieService.isLoggedIn, function(req, res) {
@@ -13,23 +14,26 @@ router.get('/', CookieService.isLoggedIn, function(req, res) {
                     ClientService.findClientsEmailedNotPacked(function(err, emailedNotPacked) {
                         ClientService.findClientsDone(function(err, completed) {
                             ClientService.findClientsNoPreparer(function(err, noPreparer) {
-                                var cookie = CookieService.readCookie(req);
-                                var clients = [
-                                    ['normal', normal],
-                                    ['noPreparer', noPreparer],
-                                    ['packed', packed],
-                                    ['emailed', emailed],
-                                    ['osPyt', pickedUp],
-                                    ['emailedNotPacked', emailedNotPacked]
-                                ];
+                                UserService.getInitials(function(err, initials) {
 
-                                if (cookie.role == 'Administrator') clients.push(['done', completed]);
+                                    var cookie = CookieService.readCookie(req);
+                                    var clients = [
+                                        ['normal', normal],
+                                        ['noPreparer', noPreparer],
+                                        ['packed', packed],
+                                        ['emailed', emailed],
+                                        ['osPyt', pickedUp],
+                                        ['emailedNotPacked', emailedNotPacked]
+                                    ];
 
-                                res.render('t1/monitoring_sheet', {
-                                    title: 'T1 Monitoring',
-                                    clients: clients,
-                                    options: ClientService.getPayments(),
-                                    role: cookie.role
+                                    if (cookie.role == 'Administrator') clients.push(['done', completed]);
+
+                                    res.render('t1/monitoring_sheet', {
+                                        title: 'T1 Monitoring',
+                                        clients: clients,
+                                        options: ClientService.getPayments(),
+                                        role: cookie.role,
+                                    }); 
                                 });
                             });
                         });
