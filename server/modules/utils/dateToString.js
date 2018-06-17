5a6de587c4b7329440cd8b4e606f68e40f5a5404
Anders <anders.yb.leung@gmail.com>
2018-06-16 22:48:39 -0700

    Update datetostring.

--- server/modules/utils/dateToString.js
+++ server/modules/utils/dateToString.js
@@ -6,8 +6,12 @@ module.exports = function(date) {
     var month = date.getMonth() + 1;
     var year = date.getFullYear();
 
-    if (month.length != 2) {
+    if (month.toString().length == 1) {
         month = '0' + month;
+    }
+
+    if (day.toString().length == 1) {
+        day = '0' + day;
     }
 
     return year + '-' + month + '-' + day;
