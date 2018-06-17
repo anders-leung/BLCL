5508bc48ca2fbd135f038a0a55df94cb3a0fd85f
Anders <anders.yb.leung@gmail.com>
2018-06-16 22:49:39 -0700

    Email setup.

---
+++ server/modules/email/templates/packed_not_pu.js
@@ -0,0 +1,5 @@
+module.exports = function template(client) {
+    subject: 'Your T1 is packed and ready to be picked up'
+    text: 'Your T1 is packed and ready to be picked up.'
+    html: '<p>Your T1 is packed and ready to be picked up.</p>'
+}
