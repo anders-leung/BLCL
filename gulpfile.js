c0c6be8db092bc41cddbd21db0cf28a25c78dd5b
Anders <anders.yb.leung@gmail.com>
2018-06-16 22:00:16 -0700

    gulp for dev setup and add packages.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync');
+
+gulp.task('default', function() {
+    nodemon({
+        script: './server/server',
+        // this listens to changes in any of these files/routes and restarts the application
+        watch: ["./server/server", "app.js", "routes/", "server/"],
+        ext: 'js'
+        // Below i'm using es6 arrow functions but you can remove the arrow and have it a normal .on('restart', function() { // then place your stuff in here }
+    }).on('restart', () => {
+        gulp.src('./server/server')
+    });
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        proxy: 'localhost:3000'
+    });
+    gulp.watch('./client/*/**').on('change', browserSync.reload);
+});
