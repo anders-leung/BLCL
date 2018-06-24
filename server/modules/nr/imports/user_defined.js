34ad81b7b001bc9c9bd289ede9955268f04d5f12
Anders <anders.yb.leung@gmail.com>
2018-06-24 16:16:19 -0700

    Change importing.

--- server/modules/nr/imports/user_defined.js
+++ server/modules/nr/imports/user_defined.js
@@ -1,20 +1,18 @@
 let XLSX = require('xlsx-populate');
-let fs = require('fs');
 
 let Client = require('../../../models/nr/NR');
-let Agent = require('../../../models/nr/Contact');
 
 let clientNames = require('./client');
 
 let fields = require('./user_defined_fields');
 
 function loadFromExcel(filepath) {
-    let workbook = XLSX.fromFileAsync(filepath).then(workbook => {
+    XLSX.fromFileAsync(filepath).then((workbook) => {
         let sheet = workbook.sheet('Address Book');
         let row = 2;
         let value = sheet.row(row).cell(8).value();
         while (value) {
-            if ((value == 'Active' || value == 'Pending') && sheet.row(row).cell(3).value() != 'AG') readClient(sheet, row);
+            if (value == 'Active' && sheet.row(row).cell(3).value() != 'AG') readClient(sheet, row);
             row++;
             value = sheet.row(row).cell(8).value();
         }
@@ -24,23 +22,19 @@ function loadFromExcel(filepath) {
 function readClient(sheet, row) {
     let client = new Client();
     client.properties = [];
-    client.cc.completionDates = [];
-    let agent = new Agent();
     for (let col = 1; col < 85; col++) {
         let value = sheet.row(row).cell(col).value();
         if (!value) continue;
         value = value.toString().toUpperCase();
         if ((col > 14 && col < 21) || col == 23) value = { street: value };
-        let model = client;
-        if (col > 21 && col < 30) model = agent;
-        if (fields[col]) fields[col](value, model);
+        if (fields[col]) fields[col](value, client);
     }
 
     client.name = clientNames.getFileName(client);
     client.pathName = clientNames.getPathName(client);
     client.year = 2018;
     client.save(err => {
-        if (err) console.log('err: ', err);
+        if (err) console.log(err);
     });
 }
 
