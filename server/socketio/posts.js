0f4ba8a383be0b484de6ee3dcd8e3f53c6618fb1
Anders <anders.yb.leung@gmail.com>
2018-06-24 22:20:09 -0700

    Tack on NR socket handling.

--- server/socketio/posts.js
+++ server/socketio/posts.js
@@ -1,14 +1,15 @@
 /**
  * Created by ander on 2017-11-22.
  */
-var ClientService = require('../modules/t1/client');
+let T1Service = require('../modules/t1/client');
+let NRService = require('../modules/nr/client');
 
-var ClientsSocket = {
-    ClientUpdate : function(socket) {
+let ClientsSocket = {
+    ClientUpdate : async function(socket) {
         socket.on('client side update', function (data) {
             console.log(data);
             if (data.field == 'preparer') {
-                ClientService.findClient({ fileName : data.fileName }, function(err, client) {
+                T1Service.findClient({ fileName : data.fileName }, function(err, client) {
                     data.client = client[0];
                     socket.broadcast.emit('job assignment', data);
                 })
@@ -16,17 +17,27 @@ var ClientsSocket = {
             socket.broadcast.emit('client side update', data);
 
             if (data.field == 'packed') data.value = data.value == 'Y';
-            var search = {};
+            let search = {};
             search['fileName'] = data.fileName;
-            var update = {};
+            let update = {};
             update[data.field] = data.value;
             
-            ClientService.updateClient(search, update, false, function(err, client) {
-                if (err) {
-                    console.log(err);
-                }
+            T1Service.updateClient(search, update, false, function(err, client) {
+                if (err) console.log(err);
             });
         });
+
+        socket.on('nr update', async function(data) {
+            socket.broadcast.emit('nr update', data);
+
+            let search = { name: data.fileName, year: data.year };
+            let update = {};
+            update[data.service + '.' + data.field] = data.value;
+
+            let err, result;
+            [err, result] = await NRService.update(search, update);
+            if (err) console.log(err);
+        })
     }
 };
 
