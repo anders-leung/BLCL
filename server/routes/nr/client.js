4051cc9495eb45f4294c4a0c1b9a737cab3f73c4
Anders <anders.yb.leung@gmail.com>
2018-07-01 14:28:53 -0700

    Add posting for nr client.

--- server/routes/nr/client.js
+++ server/routes/nr/client.js
@@ -17,7 +17,6 @@ router.get('/', CookieService.isLoggedIn, function(req, res) {
 router.get('/:client_name', CookieService.isLoggedIn, async (req, res) => {
     let err, client;
     [err, client] = await ClientService.find({ pathName: req.params.client_name });
-    console.log(client)
     res.render('nr/client', {
         title: 'NR Client',
         role: req.session.role,
@@ -25,5 +24,19 @@ router.get('/:client_name', CookieService.isLoggedIn, async (req, res) => {
         client: client[0]
     });
 });
+
+router.post('/', CookieService.isLoggedIn, async (req, res, next) => {
+    let err, result;
+    [err, result] = await ClientService.create(req.body);
+    if (err) return next(err);
+    res.redirect('/nr/directory');
+});
+
+router.post('/:client_name', CookieService.isLoggedIn, async (req, res, next) => {
+    let err, result;
+    [err, result] = await ClientService.update({ pathName: req.params.client_name }, req.body);
+    if (err) return next(err);
+    res.redirect('/nr/directory');
+})
 
 module.exports = router;
