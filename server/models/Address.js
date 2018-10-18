ecea4eb3d154bb49d282ceb588df83ba37aa261d
Anders <anders.yb.leung@gmail.com>
2018-10-17 22:45:36 -0700

    Address schema full address virtual.

--- server/models/Address.js
+++ server/models/Address.js
@@ -10,4 +10,19 @@ var AddressSchema = new Schema({
     country: String
 });
 
+AddressSchema.virtual('fullAddress').get(function() {
+    let string = '';
+    if (this.apartment) string += this.apartment + ' ';
+    if (this.street) string += this.street + ', ';
+    if (this.city) string += this.city + ', ';
+    if (this.province) string += this.province + ' ';
+    if (this.postalCode) string += this.postalCode + ', ';
+    if (this.country) string += this.country;
+
+    string = string.trim();
+    string = string.replace(/[^A-z]$/, '');
+
+    return string;
+});
+
 module.exports = mongoose.model('Address', AddressSchema);
