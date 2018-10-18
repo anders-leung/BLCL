dcf1f330dca1b825e409cee4fbc3d468b79acc60
Anders <anders.yb.leung@gmail.com>
2018-10-17 22:49:33 -0700

    Add getters for config stuff.

--- server/modules/config.js
+++ server/modules/config.js
@@ -2,6 +2,7 @@
  * Created by ander on 2017-10-27.
  */
 var Config = require('../models/config');
+const to = require('../helpers/to');
 
 var ConfigService = {
     setT1Directory : function(directory, callback) {
@@ -20,6 +21,26 @@ var ConfigService = {
             if (config) callback(err, config.t1_directory);
             else callback(err, null);
         });
+    },
+
+    getInvoiceDirectory: async () => {
+        const [err, config] = await to(Config.findOne({}));
+        return [err, config.invoice_directory];
+    },
+
+    getTax: async () => {
+        const [err, config] = await to(Config.findOne({}).lean());
+        if (err) return [err, null];
+        if (!config) return [new Error('General config not set up'), null];
+        const tax = {
+            gst: config.gst || '0.05',
+            pst: config.pst || '0.07',
+        }
+        return [null, tax];
+    },
+
+    getConfig: async () => {
+        return await to(Config.findOne({}).lean());
     }
 };
 
