8407f16bd1c91013007c99f89e271668532350f6
Anders <anders.yb.leung@gmail.com>
2018-10-18 19:47:13 -0700

    Sales page.

---
+++ server/routes/invoice/sales.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+const getWeeks = require('./get_weeks');
+const CookieService = require('../utils/cookies');
+const InvoiceService = require('../../modules/invoice/invoice');
+
+/* GET home page. */
+router.get('/', CookieService.isLoggedIn, async (req, res) => {
+    const role = CookieService.readCookie(req).role;
+    if (role != 'Administrator') {
+        return res.redirect('/');
+    }
+
+    const weeks = getWeeks();
+    const query = {
+        $or: [
+            { pytReceived: '' },
+            { pytReceived: { $exists: false } },
+        ],
+    };
+
+    const services = InvoiceService.getServices();
+    const [err, invoices] = await InvoiceService.getByWeek(query, 'issueDate', weeks[0]);
+
+    if (err) return res.render('error', err);
+
+    res.render('invoice/sales', {
+        invoices,
+        weeks,
+        services,
+        role: req.session.role,
+        options: {},
+    });
+});
+
+module.exports = router;
