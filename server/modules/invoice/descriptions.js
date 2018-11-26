851919eeeca982d46091354663f95409eb42dd19
Anders <anders.yb.leung@gmail.com>
2018-11-26 01:21:04 -0800

    Descriptions json for now, will move to db probably.

---
+++ server/modules/invoice/descriptions.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'ntr': 'preparing NTR report on financial statements and corporation tax return and related schedules for the year ended <year>',
+    't1r': 'T1 Review on <year>',
+    'gst': 'preparing GST return for the period from <year> to <year>',
+    'pr': 'payroll administration for the period <year> to <year>\n' +
+            'preparing and filing T4 slips and T4 summary for the year <year>\n' +
+            'preparing and filing WCB for the year <year>\n' +
+            'issue ROE for the period <year> to <year>',
+    'con': 'consultation for <hours> hour on <year>',
+    'disc': 'less <percent>% special discount',
+    't5': 'preparing <year> T5 slips and T5 summary',
+    'nr': 'handling the change to non-resident status',
+    'can': 'compilation for <year> financial statements and corportaion tax return and related schedules for the year ended <year>',
+    't2054': 'preparing T2054 for election for a capital dividend declared in <year>'
+}
