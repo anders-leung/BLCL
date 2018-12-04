b0444a7c9de4184b34d2a6f611ea6e84ff31b748
Anders <anders.yb.leung@gmail.com>
2018-12-04 01:04:19 -0800

    Add the static descriptions I was given before.

--- server/modules/invoice/descriptions.js
+++ server/modules/invoice/descriptions.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'ntr': 'preparing NTR report on financial statements and corporation tax return and related schedules for the year ended <year>',
+    'fs': 'preparing NTR report on financial statements and corporation tax return and related schedules for the year ended <year>',
     't1r': 'T1 Review on <year>',
     'gst': 'preparing GST return for the period from <year> to <year>',
     'pr': 'payroll administration for the period <year> to <year>\n' +
@@ -9,7 +9,12 @@ module.exports = {
     'con': 'consultation for <hours> hour on <year>',
     'disc': 'less <percent>% special discount',
     't5': 'preparing <year> T5 slips and T5 summary',
-    'nr': 'handling the change to non-resident status',
+    'nr dpt': 'handling the change to non-resident status',
     'can': 'compilation for <year> financial statements and corportaion tax return and related schedules for the year ended <year>',
-    't2054': 'preparing T2054 for election for a capital dividend declared in <year>'
+    't2054': 'preparing T2054 for election for a capital dividend declared in <year>',
+    'nr 216': 'preparing <year> NR6, NR4 and non-resident tax return (Under S216) and related schedules re rental of Canadian taxable property at <address>',
+    'nr cc': 'preparing and filing certificate of compliance re disposition of Canadian taxable property at <address>',
+    'nr 116': 'preparing and filing <year> non-resident income tax return under (S116) re disposition of Canadian taxable property at <address>',
+    'nr cra': 'seeking CRA\'s confirmation that the non resident is not the beneficial owner of the property and no Clearance Certificate is required on the property transfer of Canadian taxable property at <address>',
+    't1134': ' preparing and filing foreign asset reporting T1134 for <number> controlled foreign affiliates for <year>'
 }
