9fd1fead052ab76612fb0892dfc30af4a6c24380
Anders <anders.yb.leung@gmail.com>
2018-12-05 19:09:29 -0800

    Doesn't need to be an object of functions.

--- server/socketio/invoices/description.js
+++ server/socketio/invoices/description.js
@@ -1,17 +1,15 @@
 const DescriptionService = require('../../modules/invoice/description');
 
-module.exports = {
-    update: async (socket, data) => {
-        const query = {
-            service: data.service
-        };
+module.exports = async (socket, data) => {
+    const query = {
+        service: data.service
+    };
 
-        const [err, update] = await DescriptionService.update(query, data);
+    const [err, update] = await DescriptionService.update(query, data);
 
-        if (err) {
-            return console.log('socket description update err: ', err)
-        } else {
-            socket.emit('description update', update);
-        }
+    if (err) {
+        return console.log('socket description update err: ', err)
+    } else {
+        socket.emit('description update', update);
     }
 }
