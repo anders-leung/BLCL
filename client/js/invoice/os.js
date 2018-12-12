6bdbad8d32e0ccc69e25ab36c6f8fd106a6c3f39
Anders <anders.yb.leung@gmail.com>
2018-12-11 19:47:32 -0800

    Change export title.

--- client/js/invoice/os.js
+++ client/js/invoice/os.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         'buttons': [{
             extend: 'excelHtml5',
             footer: true,
-            title: `Payments Received From ${$('#weeks').val()}`,
+            title: `O/S Invoices From ${$('#weeks').val()}`,
             exportOptions: {
                 columns: ':visible'
             }
