6fc48f2e4c9e7eb6107a480c24558118f0d82174
Anders <anders.yb.leung@gmail.com>
2018-12-11 19:49:55 -0800

    Update the year end values instead of being excel number representations.

---
+++ server/modules/clients/yearEnd.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const XLSX = require('xlsx-populate');
+const ClientService = require('./client');
+
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/test');
+
+function loadFromExcel(filepath) {
+    XLSX.fromFileAsync(filepath).then(workbook => {
+        let sheet = workbook.sheet('Address Book');
+        let row = 2;
+        let stopperCell = sheet.row(row).cell(1);
+        while (stopperCell.value()) {
+            readClient(sheet, row);
+            row++;
+            stopperCell = sheet.row(row).cell(1);
+        }
+    });
+}
+
+async function readClient(sheet, row) {
+    function getValue(col) {
+        let value = sheet.row(row).cell(col).value();
+        if (!value) return '';
+        value = value.toString();
+        return value;
+    }
+
+    const name = getValue(1);
+    let yearEnd = getValue(7);
+    yearEnd = XLSX.numberToDate(yearEnd);
+    
+    await ClientService.update({ name }, { yearEnd });
+}
+
+const args = process.argv.slice(2);
+loadFromExcel(args[0]);
