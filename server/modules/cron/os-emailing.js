b66a0e61eef9aa0d9aa1cea223c85ba70831917b
Anders <anders.yb.leung@gmail.com>
2018-12-16 15:31:26 -0800

    Cron job for emailing clients with os invoices.

---
+++ server/modules/cron/os-emailing.js
@@ -0,0 +1,42 @@
+const schedule = require('node-schedule');
+
+const frequency = '* 0 * * * *';
+const day = 1000 * 60 * 60 * 24;
+
+const notPaid = {
+    $or: [
+        { pytReceived: '' },
+        { pytReceived: { $exists: false } },
+    ]
+};
+
+const neverEmailed = Object.assign({
+    signDate: {
+        $lte: new Date(new Date() - day * 5),
+    },
+    emailed: {
+        $exists: false
+    },
+}, notPaid);
+
+const emailedBefore = Object.assign({
+    signDate: {
+        $exists: true,
+    },
+    'emailed.when': {
+        $lte: new Date(new Date() - day * 10),
+    },
+    'emailed.attempt': {
+        $gte: 1,
+    }
+}, notPaid);
+
+const firstEmail = schedule.scheduleJob(frequency, () => {
+    const osEmail = require('../invoice/os-emailing');
+    osEmail(neverEmailed)
+});
+
+const otherEmail = schedule.scheduleJob(frequency, () => {
+    const osEmail = require('../invoice/os-emailing');
+    osEmail(emailedBefore)
+});
