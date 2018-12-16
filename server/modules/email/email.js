5aeca1774bdc1a6f49f241e28626e9fc81f2d73c
Anders <anders.yb.leung@gmail.com>
2018-12-16 01:51:41 -0800

    Update emailing module.

--- server/modules/email/email.js
+++ server/modules/email/email.js
@@ -1,24 +1,33 @@
+const jade = require('jade');
 const nodemailer = require('nodemailer');
 
-module.exports = function emailClient(user, client, template, values) {
+module.exports = function emailClient(user, client, template, values, options) {
     let transporter = nodemailer.createTransport({
         host: 'smtp.1and1.com',
         port: 587,
         secure: false,
         auth: {
             user: user.email,
-            pass: user.password
+            pass: user.emailPassword
         }
     });
     
+    const htmlFile = `${__dirname}/templates/${template}.jade`;
+    const textFile = `${__dirname}/templates/${template}.txt`;
+
+    const htmlFn = jade.compileFile(htmlFile);
+    const textFn = jade.compileFile(textFile);
+
+    const html = htmlFn(values);
+    const text = textFn(values);
+    
     let mailOptions = {
-        from: '"' + user.name + '" <' + user.email + '>',
-        to: client.email
+        from: user.name + '<' + user.email + '>',
+        to: client.email,
+        html,
+        text,
     }
-
-    let template = require('./templates/' + template);
-
-    mailOptions = Object.assign(mailOptions, template(client));
+    mailOptions = Object.assign(mailOptions, options);
     
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
