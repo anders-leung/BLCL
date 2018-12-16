78eca5d0acf091da72063d86be6e1e8ce357f8e1
Anders <anders.yb.leung@gmail.com>
2018-12-16 15:32:39 -0800

    Move cron logic out, and create a dict when emailing, for faster lookup when emailing many clients.

--- server/modules/invoice/os-emailing.js
+++ server/modules/invoice/os-emailing.js
@@ -1,57 +1,89 @@
-const schedule = require('node-schedule');
-
 const UserService = require('../user');
 const InvoiceService = require('../invoice/invoice');
 const emailClient = require('../email/email');
 
-const email = schedule.scheduleJob('* 0 * * * *', osEmail);
-
-async function osEmail() {
-    const firstEmail = {
-        signDate: {
-            $lte: new Date(new Date() - 1000 * 60 * 60 * 24 * 10),
-        },
-        emailed: {
-            $exists: false
-        }
-    };
-
-
+async function osEmail(query) {
     let err, invoices;
 
-    [err, invoices] = await InvoiceService.get(firstEmail);
+    [err, invoices] = await InvoiceService.get(query);
 
     if (err) return console.log('osEmailing err: ', err);
 
-    invoices.map(async (invoice) => {
-        let client = invoice.client;
-        if (!client) {
-            client = invoice.oneTimeClient;
-        }
-
-        const [err, user] = await UserService.findOneUser({ initials: invoice.issuedBy });
+    UserService.getAllUsers((err, users) => {
         if (err) return console.log('Invoice error: ', err);
 
-        const values = {
-            attention: client.contactString || client.name,
-            number: invoice.number,
-            amount: invoice.total,
-            date: invoice.issueDate.toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' }),
-            user: user.name,
-            company: invoice.company,
-        }
+        const userDict = {};
+        users.map((user) => {
+            const { initials } = user;
+            userDict[initials] = user;
+        });
+        invoices.map(async (invoice) => {
+            let client = invoice.client;
+            if (!client) {
+                client = invoice.oneTimeClient;
+            }
 
-        const options = {
-            subject: 'Benjamin Leung & Co., Ltd Outstanding Invoice',
-            attachments: [{
-                filename: `Invoice ${invoice.number}.pdf`,
-                path: `${global.invoiceDirectory}/${invoice.company.toUpperCase()}/${invoice.number}.pdf`,
-                contentType: 'application/pdf'
-            }]
-        }
+            const user = userDict[invoice.issuedBy];
 
-        emailClient(user, client, 'os', values, options);
+            const values = {
+                attention: client.contactString || client.name,
+                number: invoice.number,
+                amount: invoice.total,
+                date: invoice.issueDate.toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' }),
+                user: user.name,
+                company: invoice.company,
+            }
+
+            let reminder = '1st';
+            if (invoice.emailed) {
+                const attempt = invoice.emailed.attempt;
+                reminder = ordinalSuffixOf(attempt + 1);
+            }
+            reminder += ' Reminder';
+    
+            const options = {
+                subject: `Benjamin Leung & Co., Ltd Outstanding Invoice, ${reminder}`,
+                attachments: [{
+                    filename: `Invoice ${invoice.number}.pdf`,
+                    path: `${global.invoiceDirectory}/${invoice.company.toUpperCase()}/${invoice.number}.pdf`,
+                    contentType: 'application/pdf'
+                }],
+                cc: 'Accounts Receivable <ar@ben-cpa.com>',
+            }
+
+            emailClient(user, client, 'os', values, options, async (err, success) => {
+                if (success) {
+                    let attempt = 1;
+                    if (invoice.emailed && invoice.emailed.attempt) {
+                        attempt = invoice.emailed.attempt + 1;
+                    }
+                    const query = { _id: invoice._id };
+                    const update = {
+                        emailed: {
+                            when: new Date(),
+                            attempt,
+                        }
+                    };
+                    await InvoiceService.update(query, update);
+                }
+            });
+        });
     });
+}
+
+function ordinalSuffixOf(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
 }
 
 module.exports = osEmail;
