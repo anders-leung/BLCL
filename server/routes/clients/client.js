06c2c73ab67ab2b0c213a9a8dee94c2f21ae01b4
Anders <anders.yb.leung@gmail.com>
2018-12-24 15:44:44 -0800

    Get all invoices of a client.

--- server/routes/clients/client.js
+++ server/routes/clients/client.js
@@ -4,17 +4,23 @@ const router = express.Router();
 const CookieService = require('../utils/cookies');
 const ClientService = require('../../modules/clients/client');
 const DescriptionService = require('../../modules/invoice/description');
+const InvoiceService = require('../../modules/invoice/invoice');
 
 router.get('/*', CookieService.isLoggedIn, async function(req, res) {
-    let err, client;
+    let err, client, invoices;
     const { params } = req;
     if (params[0]) {
-        const query = { _id: params[0] };
+        let query = { _id: params[0] };
     
         [err, clients] = await ClientService.get(query);
         if (clients.length === 1) {
             client = clients[0];
         }
+
+        if (err) return res.render('error', err);
+
+        query = { client: client._id };
+        [err, invoices] = await InvoiceService.get(query);        
     }
 
     if (err) return res.render('error', err);
@@ -24,6 +30,7 @@ router.get('/*', CookieService.isLoggedIn, async function(req, res) {
     res.render('clients/client', {
         client,
         services,
+        invoices,
         role: req.session.role,
         options: {}
     });
