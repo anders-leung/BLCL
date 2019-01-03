3d2b7c113581aa9c4114a9125127ba8576619f2b
Anders <anders.yb.leung@gmail.com>
2019-01-02 18:30:50 -0800

    Templates route.

---
+++ server/routes/clients/templates.js
@@ -0,0 +1,27 @@
+/**
+ * Created by ander on 2018-12-24.
+ */
+
+const express = require('express');
+const router = express.Router();
+
+const CookieService = require('../utils/cookies');
+const TemplateService = require('../../modules/template/template');
+
+/* GET home page. */
+router.get('/*', CookieService.isLoggedIn, async (req, res) => {
+    const { params } = req;
+    
+    const [err, templates] = await TemplateService.get();
+    
+    if (err) return res.render('error');
+
+    res.render('clients/templates', {
+        templates,
+        role: req.session.role,
+        options: {}
+    });
+});
+
+
+module.exports = router;
