3f57769423ccaaf476095ec91b59cdf335f2d15a
Anders <anders.yb.leung@gmail.com>
2019-01-06 14:29:23 -0800

    Fix up the module, need a setup maybe, to populate based on services.

--- server/modules/t2/client.js
+++ server/modules/t2/client.js
@@ -1,17 +1,19 @@
-const ClientService = require('../clients/client');
-const Company = require('../../models/t2/Company');
 const to = require('../../helpers/to');
+const Client = require('../../models/client/Client');
+const T2 = require('../../models/t2/T2');
 
 const T2Service = {
-    create: async (data) => {
-        // const [err, client] = await ClientService.create(getClient(data));
-        // if (err) return err;
-        // data.client = client;
-        return await to(new Company(data).save());
+    get: async (query) => {
+        return await to(T2.find(query));
     },
 
-    get: async (query) => {
-        return await to(Company.find(query));
+    update: async (query, update) => {
+        return await to(T2.findOneAndUpdate(query, update, { new: true, upsert: true }));
+    },
+
+    setup: async () => {
+        const [err, clients] = await to(Client.find({ services: 'FS' }));
+        
     }
 }
 
