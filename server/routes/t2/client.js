eb770158a37fa133dbe4904792eb5637ed596981
Anders <aleung@dosarrest.com>
2019-01-06 20:37:12 -0800

    Update the client page, use one route with wildcard.

--- server/routes/t2/client.js
+++ server/routes/t2/client.js
@@ -2,58 +2,44 @@ const express = require('express');
 const router = express.Router();
 
 const CookieService = require('../utils/cookies');
-const ClientService = require('../../modules/t2/client');
+const ClientService = require('../../modules/clients/client');
+const T2Service = require('../../modules/t2/client');
 const UserService = require('../../modules/user');
 
-router.get('/', CookieService.isLoggedIn, async function(req, res) {
-    const [err, initials] = await UserService.getInitials();
+router.get('/*', CookieService.isLoggedIn, async (req, res) => {
+    let err, t2, initials, clients;
+    const { params } = req;
+    const { _id } = params;
+    const query = { _id };
+
+    if (_id) {
+        [err, t2] = await T2Service.find(query);
+    }
+
+    [err, initials] = await UserService.getInitials();
+    [err, clients] = await ClientService.get({});
     if (err) return res.render('error')
 
     res.render('t2/client', { title: 'T2 Interview',
-        client : null,
+        t2,
+        clients,
         role : CookieService.readCookie(req).role,
         options : {
-            initials: initials
-        },
-        newClient : true
-    });
-});
-
-router.get('/:id', CookieService.isLoggedIn, async (req, res) => {
-    const id = req.params.id;
-    let err, clients, intiials;
-    [err, client] = await ClientService.get({ _id: id });
-    [err, initials] = await UserService.getInitials();
-    res.render('t2/client', {
-        title : 'T2 Interview',
-        client : client[0],
-        role : CookieService.readCookie(req).role,
-        options : {
-            initials: initials
-        },
-        newClient : false
-    });
-});
-
-router.post('/', function(req, res) {
-    req.body.new = true;
-    ClientService.create(req.body, function(err, client) {
-        if (err) {
-            res.render('error');
-        } else {
-            res.redirect('/t1/directory');
+            initials
         }
     });
 });
 
-router.post('/:client_name', function(req, res) {
-    const client_name = req.params.client_name;
-    ClientService.updateClient({ pathName : client_name }, req.body, true, function(err, client) {
-        if (err) {
-            res.render('error');
-        }
-        res.redirect('/t1/directory');
-    })
+router.post('/*', async (req, res) => {
+    let err, client;
+    const { body } = req;
+    const query = { client: body.client };
+
+    [err, client] = await T2Service.update(query, req.body);
+
+    if (err) return res.render('error', err);
+
+    res.redirect(`/t2/client/${client._id}`);
 });
 
 module.exports = router;
