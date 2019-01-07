d692ebd124ebbfeb4274d9c2882904fc8f5add82
Anders <aleung@dosarrest.com>
2019-01-06 20:34:09 -0800

    Add client info templating as services may need client info.

---
+++ client/js/templates/client.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('#clientString').autocomplete({
+        source: clients.map((client) => {
+            return {
+                label: client.name,
+                value: client._id,
+            }
+        }),
+        focus: function(event, ui) {
+            event.preventDefault();
+            $(this).val(ui.item.label);
+            $('#name').val(ui.item.value);
+            updateUI(ui.item.value);
+        },
+        select: function(event, ui) {
+            event.preventDefault();
+            $(this).val(ui.item.label);
+            $('#name').val(ui.item.value);
+            updateUI(ui.item.value);
+        },
+    });
+
+    function updateUI(id) {
+        clients.map((client) => {
+            if (client._id === id) {
+                $('#status options').each(function () {
+                    var value = $(this).val();
+                    if (client.status === value) {
+                        $(this).attr('selected', true);
+                    }
+                });
+                $('#newClientDate').val(client.newClientDate);
+                $('#title').val(client.contact.title);
+                $('#firstName').val(client.contact.firstName);
+                $('#lastName').val(client.contact.lastName);
+                $('#phone').val(client.phone);
+                $('#fax').val(client.fax);
+                $('#email').val(client.email);
+                $('#addressApartment').val(client.address.apartment);
+                $('#addressStreet').val(client.address.street);
+                $('#addressCity').val(client.address.city);
+                $('#addressProvince').val(client.address.province);
+                $('#addressCountry').val(client.address.country);
+                $('#addressPostalCode').val(client.address.postalCode);
+            }
+        });
+    }
+});
