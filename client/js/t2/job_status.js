816f61a7c5ec0db2c3f2b0df61b64e1c8f1aedba
Anders <anders.yb.leung@gmail.com>
2019-01-13 16:12:09 -0800

    Follow common file naming conventions.

--- client/js/t2/job_status.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by ander on 2017-12-04.
- */
-$(document).ready(function() {
-    $('table').DataTable();
-});
---
+++ client/js/t2/monitoring.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ander on 2017-12-04.
+ */
+$(document).ready(function() {
+    $('table').DataTable();
+});
