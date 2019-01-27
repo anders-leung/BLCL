fe946262546cc78ad3c4cadfb52764f8d04c4e55
Anders <anders.yb.leung@gmail.com>
2019-01-26 16:14:42 -0800

    Change setup to use service model ids instead of strings.

--- server/modules/t2/client.js
+++ server/modules/t2/client.js
@@ -1,6 +1,7 @@
 const to = require('../../helpers/to');
 const Client = require('../../models/client/Client');
 const T2 = require('../../models/t2/T2');
+const Service = require('../../models/client/Service');
 
 const T2Service = {
     get: async (query) => {
@@ -13,13 +14,21 @@ const T2Service = {
     },
 
     setup: async () => {
-        let err, clients, anyT2s;
+        let err, services, clients, anyT2s;
         [err, anyT2s] = await T2Service.get({});
 
         if (anyT2s.length > 0) return [null, null];
 
-        const t2Services = ['FS', 'GST', 'BK'];
-        [err, clients] = await to(Client.find({ services: { $in: t2Services } }));
+        services = ['FS', 'GST', 'BK'];
+        services = services.map((service) => {
+            return new RegExp(service);
+        });
+        [err, services] = await to(Service.find({ name: { $in: services } }));
+
+        services = services.map((service) => {
+            return service._id;
+        });
+        [err, clients] = await to(Client.find({ services: { $in: services } }));
         if (err) return [err, null];
 
         clients.map((client) => {
