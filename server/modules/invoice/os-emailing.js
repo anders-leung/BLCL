072724492501810b85c2714df0b4758bb44eb7e2
Anders <anders.yb.leung@gmail.com>
2019-02-18 17:32:12 -0800

    Use ar account.

--- server/modules/invoice/os-emailing.js
+++ server/modules/invoice/os-emailing.js
@@ -1,6 +1,6 @@
 const UserService = require('../user');
 const InvoiceService = require('../invoice/invoice');
-const emailClient = require('../email/email');
+const emailClient = require('../notifications/email');
 
 async function osEmail(query) {
     let err, invoices;
@@ -11,58 +11,54 @@ async function osEmail(query) {
 
     if (process.env.NODE_ENV === 'test') return;
 
-    UserService.findOneUser({ email: 'ar@ben-cpa.com' }, (err, user) => {
-        if (err || !user) return console.log('Invoice error: ', err || new Error('No AR user'));
+    invoices.map(async (invoice) => {
+        let client = invoice.client;
+        if (!client) {
+            client = invoice.oneTimeClient;
+        }
 
-        invoices.map(async (invoice) => {
-            let client = invoice.client;
-            if (!client) {
-                client = invoice.oneTimeClient;
-            }
+        const values = {
+            attention: client.contactString || client.name,
+            number: invoice.number,
+            amount: invoice.total,
+            date: invoice.issueDate.toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' }),
+            user: global.ar.name,
+            company: invoice.company,
+        }
 
-            const values = {
-                attention: client.contactString || client.name,
-                number: invoice.number,
-                amount: invoice.total,
-                date: invoice.issueDate.toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' }),
-                user: user.name,
-                company: invoice.company,
-            }
+        let reminder = '1st';
+        if (invoice.emailed) {
+            let attempt = invoice.emailed.attempt;
+            if (!attempt) attempt = 0;
+            reminder = ordinalSuffixOf(attempt + 1);
+        }
+        reminder += ' Reminder';
 
-            let reminder = '1st';
-            if (invoice.emailed) {
-                let attempt = invoice.emailed.attempt;
-                if (!attempt) attempt = 0;
-                reminder = ordinalSuffixOf(attempt + 1);
-            }
-            reminder += ' Reminder';
-    
-            const options = {
-                subject: `Benjamin Leung & Co., Ltd Outstanding Invoice, ${reminder}`,
-                attachments: [{
-                    filename: `Invoice ${invoice.number}.pdf`,
-                    path: `${global.invoiceDirectory}/${invoice.company.toUpperCase()}/${invoice.number}.pdf`,
-                    contentType: 'application/pdf'
-                }],
-                cc: 'Accounts Receivable <ar@ben-cpa.com>',
-            }
+        const options = {
+            subject: `Benjamin Leung & Co., Ltd Outstanding Invoice, ${reminder}`,
+            attachments: [{
+                filename: `Invoice ${invoice.number}.pdf`,
+                path: `${global.invoiceDirectory}/${invoice.company.toUpperCase()}/${invoice.number}.pdf`,
+                contentType: 'application/pdf'
+            }],
+            cc: 'Accounts Receivable <ar@ben-cpa.com>',
+        }
 
-            emailClient(user, client, 'os', values, options, async (err, success) => {
-                if (success) {
-                    let attempt = 1;
-                    if (invoice.emailed && invoice.emailed.attempt) {
-                        attempt = invoice.emailed.attempt + 1;
-                    }
-                    const query = { _id: invoice._id };
-                    const update = {
-                        emailed: {
-                            when: new Date(),
-                            attempt,
-                        }
-                    };
-                    await InvoiceService.update(query, update);
+        emailClient(global.ar, client, 'os-invoice', values, options, async (err, success) => {
+            if (success) {
+                let attempt = 1;
+                if (invoice.emailed && invoice.emailed.attempt) {
+                    attempt = invoice.emailed.attempt + 1;
                 }
-            });
+                const query = { _id: invoice._id };
+                const update = {
+                    emailed: {
+                        when: new Date(),
+                        attempt,
+                    }
+                };
+                await InvoiceService.update(query, update);
+            }
         });
     });
 }
